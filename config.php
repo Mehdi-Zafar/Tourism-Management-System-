38dbde1adc069ccce9c00510b80745de8f7717c9
Mehdi-Zafar <96488568+Mehdi-Zafar@users.noreply.github.com>
2022-03-22 01:41:26 +0500

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$databaseHost = '127.0.0.1'; //or localhost
-$databaseName = 'tourism'; // your db_name
-$databaseUsername = 'root'; // root by default for localhost 
-$databasePassword = '';  // by defualt empty for localhost
-
-$con = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
